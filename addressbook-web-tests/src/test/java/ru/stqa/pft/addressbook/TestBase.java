a9e56c8f190f361b22ec2df8959a69263f86f758
Anna.Lesovikova <alesovikova@gmail.com>
2020-10-13 16:33:59 +0300

    добавлен класс TestBase добавлен тест GroupDeletionTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/TestBase.java
@@ -0,0 +1,69 @@
+package ru.stqa.pft.addressbook;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+    FirefoxDriver wd;
+
+    @Before
+    public void setUp() {
+      wd = new FirefoxDriver();
+      wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+      wd.get("http://localhost:8181/addressbook/");
+      login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+      wd.findElement(By.name("user")).click();
+      wd.findElement(By.name("user")).clear();
+      wd.findElement(By.name("user")).sendKeys(username);
+      wd.findElement(By.name("pass")).click();
+      wd.findElement(By.name("pass")).clear();
+      wd.findElement(By.name("pass")).sendKeys(password);
+      wd.findElement(By.cssSelector("input:nth-child(7)")).click();
+    }
+
+    @After
+    public void tearDown() {
+      wd.quit();
+    }
+
+    protected void returnToGroupPage() {
+      wd.findElement(By.linkText("group page")).click();
+    }
+
+    protected void submitGroupCreation() {
+      wd.findElement(By.name("submit")).click();
+    }
+
+    protected void fillGroupForm(GroupData groupData) {
+      wd.findElement(By.name("group_name")).click();
+      wd.findElement(By.name("group_name")).sendKeys(groupData.getName());
+      wd.findElement(By.name("group_header")).click();
+      wd.findElement(By.name("group_header")).sendKeys(groupData.getHeader());
+      wd.findElement(By.name("group_footer")).click();
+      wd.findElement(By.name("group_footer")).sendKeys(groupData.getFooter());
+    }
+
+    protected void initGroupCreation() {
+      wd.findElement(By.name("new")).click();
+    }
+
+    protected void gotoGroupPage() {
+      wd.findElement(By.linkText("GROUPS")).click();
+    }
+
+    protected void deleteSelectedGroups() {
+      wd.findElement(By.name("delete")).click();
+    }
+
+    protected void selectGroup() {
+      wd.findElement(By.name("selected[]")).click();
+    }
+}
