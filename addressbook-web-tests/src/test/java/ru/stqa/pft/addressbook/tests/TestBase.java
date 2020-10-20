b754de6c69f1282c812ed187c1c6b4b3b2362735
Anna.Lesovikova <alesovikova@gmail.com>
2020-10-20 12:36:33 +0300

    Добавлена возможность запуска тестов в разных браузерах

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -1,12 +1,13 @@
 package ru.stqa.pft.addressbook.tests;
 
+import org.openqa.selenium.remote.BrowserType;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 import org.junit.After;
 import org.junit.Before;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager();
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
 
     @Before
     public void setUp() {
