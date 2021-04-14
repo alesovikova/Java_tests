8f0ab7ea989777d3ecc5532dd86d5482a827d798
Anna.Lesovikova <alesovikova@gmail.com>
2021-04-14 11:03:05 +0300

    Запуск в GChr

--- addressbook-web-tests/src/test/java/ru/stqa/pft/send_file/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/send_file/appmanager/ApplicationManager.java
@@ -28,7 +28,7 @@ public class ApplicationManager {
     }
 
     public void init() {
-        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Autotests-DEMO/addressbook-web-tests/src/test/drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Autotests-DEMO/addressbook-web-tests/src/test/drivers/linux/chromedriver");
         if (browser == BrowserType.CHROME) {
             wd = new ChromeDriver();
         } else if (browser == BrowserType.FIREFOX) {
