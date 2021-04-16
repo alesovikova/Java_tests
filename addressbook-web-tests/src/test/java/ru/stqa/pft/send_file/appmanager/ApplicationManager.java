c057b0aec75902761659a6ff2a675159a71a7fdc
Anna.Lesovikova <alesovikova@gmail.com>
2021-04-16 09:55:31 +0300

    Изменены chromeOptions

--- addressbook-web-tests/src/test/java/ru/stqa/pft/send_file/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/send_file/appmanager/ApplicationManager.java
@@ -30,20 +30,18 @@ public class ApplicationManager {
 
     public void init() {
 
-        //ChromeOptions chromeOptions= new ChromeOptions();
-        //chromeOptions.setBinary("/var/lib/jenkins/workspace/Autotests-DEMO/addressbook-web-tests/src/test/drivers/linux/chromedriver");
-        //System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Autotests-DEMO/addressbook-web-tests/src/test/drivers/linux/chromedriver");
-
-        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Autotests-DEMO/addressbook-web-tests/src/test/drivers/linux/geckodriver");
+        ChromeOptions chromeOptions= new ChromeOptions();
+        chromeOptions.setBinary("/var/lib/jenkins/workspace/Autotests-DEMO/addressbook-web-tests/src/test/drivers/linux/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/workspace/Autotests-DEMO/addressbook-web-tests/src/test/drivers/linux/chromedriver");
 
 
-//        chromeOptions.addArguments("--no-sandbox");
-//        chromeOptions.addArguments("--headless");
-//        chromeOptions.setExperimentalOption("useAutomationExtension", false);
-//        chromeOptions.addArguments("start-maximized"); // open Browser in maximized mode
-//        chromeOptions.addArguments("disable-infobars"); // disabling infobars
-//        chromeOptions.addArguments("--disable-extensions"); // disabling extensions
-//        chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+        chromeOptions.addArguments("--no-sandbox");
+        chromeOptions.addArguments("--headless");
+        chromeOptions.setExperimentalOption("useAutomationExtension", false);
+        chromeOptions.addArguments("start-maximized"); // open Browser in maximized mode
+        chromeOptions.addArguments("disable-infobars"); // disabling infobars
+        chromeOptions.addArguments("--disable-extensions"); // disabling extensions
+        chromeOptions.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
 
         //System.setProperty("webdriver.chrome.driver", "src/test/drivers/chromedriver.exe");
         if (browser == BrowserType.CHROME) {
